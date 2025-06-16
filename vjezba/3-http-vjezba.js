7b1b48368bbc4f219c859d4d9c984854b50cfb28
DanielBatinic <78101606+DanielBatinic@users.noreply.github.com>
2025-06-16 21:29:00 +0200

    Update 3-http-vjezba.js

--- vjezba/3-http-vjezba.js
+++ vjezba/3-http-vjezba.js
@@ -1,8 +1,8 @@
-const http = require("http")
+// const http = require("http")
 
-const server = http.createServer((req, res) => {
-    res.write("niggerius")
-    res.end()
-})
+// const server = http.createServer((req, res) => {
+//     res.write("niggerius")
+//     res.end()
+// })
 
-server.listen(2000)
+// server.listen(2000)
