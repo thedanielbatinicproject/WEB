8665896ebe10a04e8bfdb5005228e184f427fb9b
DanielBatinic <78101606+DanielBatinic@users.noreply.github.com>
2025-06-17 16:59:47 +0200

    espress init

---
+++ vjezba/5-express.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+
+app.use(express.static("./public"))
+
+// app.get("/", (req, res) => {
+//     res.sendFile(path.resolve(__dirname,"public","index.html"));
+// })
+
+app.listen(5000, () => console.log("Server listening on port 5000"))
